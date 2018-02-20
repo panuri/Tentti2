385e3853372b4afd6ce75dbf97f95f097183a451
k1601619 <k1601619@C-A4404-06>
2018-02-20 12:51:30 +0200

    Add project files.

---
+++ Tentti2/Piste.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tentti2
+{
+    class Piste
+    {
+        public string Nimi { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Piste()
+        {
+
+        }
+
+        public Piste(string n, double x , double y)
+        {
+            this.Nimi = n;
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return this.Nimi + " " + this.X + " " + this.Y;
+        }
+
+    }
+}
---
+++ Tentti2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Tentti2
+{
+
+    
+    class Program
+    {
+        static List<Piste> lista = new List<Piste>();
+     
+        static void Main(string[] args)
+        {
+            List<Piste> lista = new List<Piste>();
+            Piste p1 = new Piste("Piste_1", 12, 33);
+            Piste p2 = new Piste("Piste_2", 82, 53);
+            lista.Add(p1);
+            lista.Add(p2);
+            Console.WriteLine(lista[0]);
+            Console.WriteLine(lista[1]);
+          
+            string output = JsonConvert.SerializeObject(lista);
+            Console.WriteLine("listan pisteet JSON muodossa:");
+            Console.WriteLine(output);
+
+            LueBin();
+        }
+
+        static void LueBin()
+        {                   
+            FileStream stream = new FileStream(@"M:\VTentti\Tentti2\bindataTentti.bin", FileMode.Open);
+            BinaryReader reader = new BinaryReader(stream);
+            reader.BaseStream.Seek(0, SeekOrigin.Begin);
+            Piste fromFilePiste;
+            while (reader != null && reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                string n = reader.ReadString();
+                double x = reader.ReadDouble();
+                double y = reader.ReadDouble();
+                
+                Console.WriteLine("Pisteen nimi: " + n + " X: " + x + " Y: " + y);
+                fromFilePiste = new Piste(n, x, y);
+                lista.Add(fromFilePiste);
+                //Console.WriteLine("Position: " + reader.BaseStream.Position);
+            }
+
+            Console.WriteLine("---- LISTAn sisätä OLIDEN TULOSTUS JSON muodossa -----");
+           
+            Console.WriteLine(JsonConvert.SerializeObject(lista));
+        }
+    }
+}
+//
+// foreach (var item in lista)
+// {
+//    Console.WriteLine(item.ToString());
+// }
